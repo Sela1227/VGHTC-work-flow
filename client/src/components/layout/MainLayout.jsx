18d50f6daa7f54056b71666d6db1dc5de6c89518
Sela1227 <snako.tw@gmail.com>
2026-01-08 06:28:51 +0800

    面板消失

--- client/src/components/layout/MainLayout.jsx
+++ client/src/components/layout/MainLayout.jsx
@@ -1,9 +1,8 @@
 import { useState } from 'react';
-import { Outlet } from 'react-router-dom';
 import Navbar from './Navbar';
 import Sidebar from './Sidebar';
 
-export default function MainLayout() {
+export default function MainLayout({ children }) {
   const [sidebarOpen, setSidebarOpen] = useState(false);
 
   return (
@@ -12,9 +11,9 @@ export default function MainLayout() {
       <Sidebar isOpen={sidebarOpen} onClose={() => setSidebarOpen(false)} />
 
       {/* 主要內容區 */}
-      <main className="lg:ml-64 pt-16 min-h-screen">
+      <main className="md:ml-64 pt-16 min-h-screen">
         <div className="p-4 md:p-6 lg:p-8">
-          <Outlet />
+          {children}
         </div>
       </main>
     </div>
