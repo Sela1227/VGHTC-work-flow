23d526057837bfabc24d57ba5660b19dc5ae25d1
Sela1227 <snako.tw@gmail.com>
2026-01-08 08:22:19 +0800

    bug fix

--- client/src/services/userService.js
+++ client/src/services/userService.js
@@ -3,32 +3,32 @@ import api from './api';
 const userService = {
   async getAll() {
     const response = await api.get('/users');
-    return response.data.data;
+    return response.data;  // api interceptor 已解包，response = { success, data }
   },
 
   async getById(id) {
     const response = await api.get(`/users/${id}`);
-    return response.data.data;
+    return response.data;
   },
 
   async create(data) {
     const response = await api.post('/users', data);
-    return response.data.data;
+    return response.data;
   },
 
   async update(id, data) {
     const response = await api.put(`/users/${id}`, data);
-    return response.data.data;
+    return response.data;
   },
 
   async resetPassword(id) {
     const response = await api.post(`/users/${id}/reset-password`);
-    return response.data.data;
+    return response.data;
   },
 
   async toggleStatus(id) {
     const response = await api.delete(`/users/${id}`);
-    return response.data.data;
+    return response.data;
   },
 };
 
